26e37fea2b8657182307dd5502b9aedbcf2cf20f
Lucas Gabriel da Silva Viana <lucas.viana@h9j.com.br>
2021-10-23 16:21:53 -0300

    bugfix: resolvendo bugs de animações

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,9 +1,16 @@
 /* pega o elemento do DOM que contém a classe .container */
-const container = document.querySelector(".container");
+const container = document.querySelector(".container"),
+    photo = document.getElementById("photo")
 
 
 container.addEventListener("animationend", (event) => { /* ouve o evento "animationend" (fim de uma animação)  */
     if (event.animationName === "fade") { /* verifica se a animação é "fade" */
+
+        photo.style.visibility = "visible"
+        photo.style.animation = "fade 500ms 500ms backwards"
+
+
         container.classList.add("shake"); /* insere a animação "shake", ao fim da animação "fade" */
+
     }
 })
